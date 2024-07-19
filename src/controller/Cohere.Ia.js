24d09894478d836455b857f45bcaa611c22399c7
david18012003 <bravocarmen264@gmail.com>
2024-07-18 21:02:38 -0500

    arreglamos la validacion de la ia

--- src/controller/Cohere.Ia.js
+++ src/controller/Cohere.Ia.js
@@ -13,9 +13,10 @@ const formatResponse = (text) => {
 export const main = async (req, res) => {
   try {
     const {mensaje} = req.body
+    const mensajeMascota= `Me gustaria que todos los resultados que me des de las consultas que te pida tenga que ver con datos o informacion con respecto a mascotas si te pido informacion con cosas que no tienen que ver con mascotas me gustaria que me envies un mensaje informando que solo puedes habklar de mascotas la pregunta es la siguiente: ${mensaje}`
     const stream = await cohere.chatStream({
       model: "command-r-plus",
-      message: mensaje,
+      message: mensajeMascota,
       temperature: 0.3,
       chatHistory: [],
       promptTruncation: "AUTO",
