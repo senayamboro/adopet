c3954c2af0ad80401ed091d726e5a13521996c64
david18012003 <bravocarmen264@gmail.com>
2024-07-18 20:26:59 -0500

    Lo que hicimos fue conectar con la base de datos de internet

--- src/db/Conexion.js
+++ src/db/Conexion.js
@@ -1,12 +1,21 @@
 import { createPool } from "mysql2/promise";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-dotenv.config({path:'./src/env/.env'})
+dotenv.config({ path: './src/env/.env' });
 
 export const pool = createPool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT,
-    database:process.env.DB_DATABASE
-})
+    host: 'be5wr48gwgcnsfm4k7ii-mysql.services.clever-cloud.com',
+    user: 'urjwbpqd5bwrzc2q',
+    password: 't1Ax12V9Y8rsWXb7I7ut',
+    database: 'be5wr48gwgcnsfm4k7ii',
+    port: 3306
+});
+
+pool.getConnection()
+    .then(conn => {
+        console.log('Conexión exitosa a la base de datos');
+        conn.release();
+    })
+    .catch(err => {
+        console.error('Error conectando a la base de datos:', err.stack);
+    });
